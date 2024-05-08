da16f2f657ca09ad09cae4108786e98a3cab14d7
pripereiras86 <pripereiras86@gmail.com>
2024-05-08 22:24:44 +0000

    inserir dados

--- exercicios/1-CRUD/3-inserir-dados.sql
+++ exercicios/1-CRUD/3-inserir-dados.sql
@@ -1,4 +1,16 @@
--- Insira 3 classes salariais na tabela salario_classe
-
-
--- Insira 1 nova classe salarial, sem declarar o atributo id_salario
+insert into salario_classe(nivel, salario)
+VALUES
+('Menor Aprendiz', 1000),
+('Estagiária', 2000),
+('Analista Jr', 3000),
+('Analista Pl', 4000),
+('Analista Sr', 5000),
+('Especialista', 6000),
+('Supervisor', 7000),
+('Coordenador', 8000),
+('Gerente', 9000),
+('Gerente Sr', 10000),
+('Superrintendente', 11000),
+('Consultor', 12000),
+('Diretor', 13000),
+('Diretor Geral', 14000);
