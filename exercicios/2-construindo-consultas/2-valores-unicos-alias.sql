3d04f67d4996971e0b8095acdaf6b4c6baff34ef
pripereiras86 <pripereiras86@gmail.com>
2024-05-15 19:40:17 +0000

    Distintos

--- exercicios/2-construindo-consultas/2-valores-unicos-alias.sql
+++ exercicios/2-construindo-consultas/2-valores-unicos-alias.sql
@@ -1,7 +1,3 @@
--- Para a tabela "customers" crie uma consulta que retorne apenas os valores únicos da coluna Country
-
-
--- Na consulta anterior, adicione a coluna Company para verificar as combinações únicas entre empresas e países
-
-
--- Nesta mesma consulta, atribua um alias com título em português para cada coluna
+select distinct country as País,
+PostalCode as CEP
+from customers;
