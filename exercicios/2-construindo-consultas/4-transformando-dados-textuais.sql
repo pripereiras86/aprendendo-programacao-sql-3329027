8e18f546743c1c88491a58088ccab4bc6a50ec7a
pripereiras86 <pripereiras86@gmail.com>
2024-05-16 18:52:59 +0000

    Tratar texto

--- exercicios/2-construindo-consultas/4-transformando-dados-textuais.sql
+++ exercicios/2-construindo-consultas/4-transformando-dados-textuais.sql
@@ -1,16 +1,9 @@
--- Crie uma consulta que retorne as colunas CustomerId, FirstName, LastName e Address das pessoas consumidoras brasileiras
-
-
--- Na consulta anterior, converta o sobrenome para letras maiúsculas
-
-
--- Na consulta anterior, converta o nome para letras minúsculas
-
-
--- Na consulta anterior, adicione uma coluna cujos os dados seja o resultado da concatenação das colunas de nome e sobrenome
-
-
--- Na consulta anterior, substitua "Av." por "Avenida" na coluna endereço
-
-
--- Na consulta anterior, utilize o símbolo de coringa para construir padrões textuais
+select distinct 
+Lower (firstname) as Nome,
+upper (lastname) as Sobrenome,
+Replace (Address, 'Av.', 'Avenida') as Endereço,
+firstname|| ' ' || LastName as Nome_completo
+from 
+customers
+WHERE
+country like '%zil';
