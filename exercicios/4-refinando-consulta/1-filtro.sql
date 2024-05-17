4f5f97b13dc84dd1f03f8fb74c6bb80e6ba0fbed
pripereiras86 <pripereiras86@gmail.com>
2024-05-17 20:05:56 +0000

    consulta

--- exercicios/4-refinando-consulta/1-filtro.sql
+++ exercicios/4-refinando-consulta/1-filtro.sql
@@ -1,1 +1,10 @@
--- Crie uma consulta para realizar inner join com as tabelas "tracks", "albums" e "artists". Além disso, apresente apenas as músicas do artista que contenha "Nação" no nome e título do álbum que não seja "Da Lama Ao Caos"
+select 
+inv.invoiceid, inv.customerid, inv.total,
+invit.unitprice,invit.quantity,
+cus.company, cus.country
+FROM
+invoices as inv
+inner join invoice_items as invit 
+on inv.invoiceid = invit.invoiceid
+inner join customers as cus
+on inv.customerid = cus.customerid;
