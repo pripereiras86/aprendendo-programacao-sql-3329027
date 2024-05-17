bdd93ba2a6ab62b0dd4e6c8c23864f355d4f2311
pripereiras86 <pripereiras86@gmail.com>
2024-05-17 20:49:35 +0000

    group by

--- exercicios/4-refinando-consulta/1-filtro.sql
+++ exercicios/4-refinando-consulta/1-filtro.sql
@@ -1,6 +1,6 @@
 select 
-inv.invoiceid, inv.customerid, inv.total,
-invit.unitprice,invit.quantity,
+inv.invoiceid, inv.customerid, sum(inv.total),
+sum(invit.unitprice),sum(invit.quantity),
 cus.company, cus.country
 FROM
 invoices as inv
@@ -8,5 +8,5 @@ inner join invoice_items as invit
 on inv.invoiceid = invit.invoiceid
 inner join customers as cus
 on inv.customerid = cus.customerid
-where 
-country like 'Brazil';
+group by inv.invoiceid
+having inv.invoiceid = 98;
